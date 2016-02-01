bc51d50dc1ca2082e31b329909033ad6502e8757
baixiao <baixiao@codoon.com>
2016-02-01 17:21:02 +0800

    gps数据格式

--- protocol/gps_protocol.go
+++ protocol/gps_protocol.go
@@ -48,44 +48,52 @@ type RouteLog struct {
 }
 
 type PostData struct {
-	SportsType         int                    `json:"sports_type" form:"sports_type"`
-	OffsetText         string                 `json:"offset_text" form:"offset_text"`
-	SportsMode         int                    `json:"sportsMode" form:"sportsMode"`
-	Version            string                 `json:"version" form:"version"`
-	HistoryVersion     int                    `json:"history_version" form:"history_version"`
-	Location           string                 `json:"location" form:"location"`
-	StartTime          string                 `json:"start_time" form:"start_time"`
-	EndTime            string                 `json:"end_time" form:"end_time"`
-	ProductId          string                 `json:"product_id" form:"product_id"`
-	IsOpen             int16                  `json:"is_open" form:"is_open"`
-	IsReal             interface{}            `json:"is_real" form:"is_real"` //ios定义为bool，android定义为int，fxxk
-	IsBaidu            int16                  `json:"is_baidu" form:"is_baidu"`
-	GoalValue          interface{}            `json:"goal_value" form:"goal_value"` //ios定义为int，android定义为float，fxxk
-	GoalType           int                    `json:"goal_type" form:"goal_type"`
-	StageDes           string                 `json:"stage_des" form:"stage_des"`
-	MaxAltitude        float64                `json:"MaxAltitude" form:"MaxAltitude"`
-	MaxToPreviousSpeed float64                `json:"MaxToPreviousSpeed" form:"MaxToPreviousSpeed"`
-	TotalTime          interface{}            `json:"total_time" form:"total_time"` //ios定义为int，android定义为float，fxxk
-	TotalLength        float64                `json:"total_length" form:"total_length`
-	TotalCalories      float64                `json:"total_calories" form:"total_calories"`
-	ActivityResult     int                    `json:"activity_result" form:"activity_result"`
-	ActivityType       int                    `json:"activity_type" form:"activity_type"`
-	BaiduCloud         interface{}            `json:"baidu_cloud" form:"baidu_cloud"` //ios定义为bool，android定义为int，fxxk
-	HighestSpeedPerkm  float64                `json:"highest_speed_perkm" form:"highest_speed_perkm"`
-	CustomWords        string                 `json:"custom_words" form:"custom_words"`
-	CaloriesPerm       []interface{}          `json:"calories_per_m" form:"calories_per_m"`
-	ProgramName        string                 `json:"program_name" form:"program_name"`
-	Model              string                 `json:"model" form:"model"`
-	LastOfProgram      int                    `json:"last_of_program" form:"last_of_program"`
-	UserTimePerkm      []UseTimePerKm         `json:"usettime_per_km" form:"usettime_per_km"`
-	UserStepsPerm      []interface{}          `json:"user_steps_list_perm" form:"user_steps_list_perm"`
-	UserStepsValid     int16                  `json:"user_steps_valid" form:"user_steps_valid"`
-	AverageSpeed       float64                `json:"AverageSpeed" form:"AverageSpeed"`
-	Points             []Point                `json:"points" form:"points"`
-	HeartRate          map[string]interface{} `json:"heart_rate" form:"heart_rate"`
-	GreenwayId         string                 `json:"greenway_id" form:"greenway_id"`
-	HalfMarathon       interface{}            `json:"half_marathon" form:"half_marathon"` //ios定义为float，android定义为int，fxxk
-	Marathon           interface{}            `json:"marathon" form:"marathon"`           //ios定义为float，android定义为int，fxxk
+	SportsType               int                    `json:"sports_type" form:"sports_type"`
+	OffsetText               string                 `json:"offset_text" form:"offset_text"`
+	SportsMode               int                    `json:"sportsMode" form:"sportsMode"`
+	Version                  string                 `json:"version" form:"version"`
+	HistoryVersion           int                    `json:"history_version" form:"history_version"`
+	Location                 string                 `json:"location" form:"location"`
+	StartTime                string                 `json:"start_time" form:"start_time"`
+	EndTime                  string                 `json:"end_time" form:"end_time"`
+	ProductId                string                 `json:"product_id" form:"product_id"`
+	IsOpen                   int16                  `json:"is_open" form:"is_open"`
+	IsReal                   interface{}            `json:"is_real" form:"is_real"` //ios定义为bool，android定义为int
+	IsBaidu                  int16                  `json:"is_baidu" form:"is_baidu"`
+	GoalValue                interface{}            `json:"goal_value" form:"goal_value"` //ios定义为int，android定义为float
+	GoalType                 int                    `json:"goal_type" form:"goal_type"`
+	StageDes                 string                 `json:"stage_des" form:"stage_des"`
+	MaxAltitude              float64                `json:"MaxAltitude" form:"MaxAltitude"`
+	MaxToPreviousSpeed       float64                `json:"MaxToPreviousSpeed" form:"MaxToPreviousSpeed"`
+	TotalTime                interface{}            `json:"total_time" form:"total_time"` //ios定义为int，android定义为float
+	TotalLength              float64                `json:"total_length" form:"total_length`
+	TotalCalories            float64                `json:"total_calories" form:"total_calories"`
+	ActivityResult           int                    `json:"activity_result" form:"activity_result"`
+	ActivityType             int                    `json:"activity_type" form:"activity_type"`
+	BaiduCloud               interface{}            `json:"baidu_cloud" form:"baidu_cloud"` //ios定义为bool，android定义为int
+	HighestSpeedPerkm        float64                `json:"highest_speed_perkm" form:"highest_speed_perkm"`
+	CustomWords              string                 `json:"custom_words" form:"custom_words"`
+	CaloriesPerm             []interface{}          `json:"calories_per_m" form:"calories_per_m"`
+	ProgramName              string                 `json:"program_name" form:"program_name"`
+	Model                    string                 `json:"model" form:"model"`
+	LastOfProgram            int                    `json:"last_of_program" form:"last_of_program"`
+	UserTimePerkm            []UseTimePerKm         `json:"usettime_per_km" form:"usettime_per_km"`
+	UserStepsPerm            []interface{}          `json:"user_steps_list_perm" form:"user_steps_list_perm"`
+	UserStepsValid           int16                  `json:"user_steps_valid" form:"user_steps_valid"`
+	AverageSpeed             float64                `json:"AverageSpeed" form:"AverageSpeed"`
+	Points                   []Point                `json:"points" form:"points"`
+	HeartRate                map[string]interface{} `json:"heart_rate" form:"heart_rate"`
+	GreenwayId               string                 `json:"greenway_id" form:"greenway_id"`
+	HalfMarathon             interface{}            `json:"half_marathon" form:"half_marathon"`                             //ios定义为float，android定义为int
+	Marathon                 interface{}            `json:"marathon" form:"marathon"`                                       //ios定义为float，android定义为int
+	IsUserStopsportsAbnormal interface{}            `json:"is_user_stopsports_abnormal" form:"is_user_stopsports_abnormal"` //可能是android专用
+	ReleaseVersion           interface{}            `json:"release_version" form:"release_version"`                         //可能是android专用
+	StartDateTime            interface{}            `json:"StartDateTime" form:"StartDateTime"`                             //可能是android专用
+	EndDateTime              interface{}            `json:"EndDateTime" form:"EndDateTime"`                                 //可能是android专用
+	TotalTime                interface{}            `json:"TotalTime" form:"TotalTime"`                                     //可能是android专用
+	IsRoot                   interface{}            `json:"is_root" form:"is_root"`                                         //可能是android专用
+	Locationcount            interface{}            `json:"locationcount" form:"locationcount"`                             //可能是android专用
+	IsCrashRestore           interface{}            `json:"is_crash_restore" form:"is_crash_restore"`                       //可能是android专用
 }
 
 type Point struct {
