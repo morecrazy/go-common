66d29b36ca381f9dd2dd367872609e23e6e2f2ea
黄鹏 <huangpeng@codoon.com>
2016-02-23 11:09:42 +0800

    add etcd_config

---
+++ etcd_config.go
@@ -0,0 +1,57 @@
+package common
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"third/context"
+	etcd "third/etcd-client"
+	//"time"
+)
+
+func NewEtcdApi(addrs []string) (etcd.KeysAPI, error) {
+	cfg := etcd.Config{
+		Endpoints: addrs,
+		Transport: etcd.DefaultTransport,
+		//HeaderTimeoutPerRequest: 10 * time.Second,
+	}
+	c, err := etcd.New(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return etcd.NewKeysAPI(c), nil
+}
+
+func CfgFromEtcd(api etcd.KeysAPI, service, env string) (string, error) {
+	rsp, err := api.Get(context.Background(), etcdKey(service, env), nil)
+	if err != nil {
+		log.Printf("read config [%s:%s] from etcd error:%v", service, env, err)
+		return "", err
+	}
+
+	if rsp.Node == nil {
+		log.Printf("empty etcd node")
+		return "", errors.New("empty etcd node")
+	}
+
+	return rsp.Node.Value, nil
+}
+
+func LoadCfgFromEtcd(addrs []string, service, env string, cfg interface{}) error {
+	api, err := NewEtcdApi(addrs)
+	if err != nil {
+		return err
+	}
+
+	data, err := CfgFromEtcd(api, service, env)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal([]byte(data), cfg)
+}
+
+func etcdKey(service, env string) string {
+	return fmt.Sprintf("/config/%s/%s", service, env)
+}
---
+++ etct_config_test.go
@@ -0,0 +1,48 @@
+package common
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+type SqlCon struct {
+	MysqlConn            string
+	MysqlConnectPoolSize int
+}
+
+type FinanceServerConfigure struct {
+	FinanceDb           SqlCon
+	MallDb              SqlCon
+	RedisNetwork        string
+	RedisAddress        string
+	RedisPassword       string
+	RedisConnectTimeout int
+	RedisReadTimeout    int
+	RedisWriteTimeout   int
+	RedisMaxActive      int
+	RedisMaxIdle        int
+	RedisIdleTimeout    int
+	RedisWait           bool
+	RedisDb             string
+	GoodsServer         string
+	RPCListen           string
+	ServerMail          string
+	FinanceAdminMails   string
+	LogDir              string
+	LogFile             string
+}
+
+func TestLoadCfg(t *testing.T) {
+	start := time.Now()
+	cfg := &FinanceServerConfigure{}
+	addrs := []string{"http://120.26.17.34:4001"}
+	err := LoadCfgFromEtcd(addrs, "financeserver", "dev", cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func(start time.Time) {
+		fmt.Println(time.Now().Sub(start))
+	}(start)
+	fmt.Printf("%+v\n", cfg)
+}
