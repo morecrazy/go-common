d32c897a12d802b765356619380bf94aa706b481
彭近尧 <Archilleus@jiangfudayin.local>
2016-03-25 14:27:51 +0800

    add session_client

---
+++ session_client.go
@@ -0,0 +1,65 @@
+package common
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type SessionStatus struct {
+	State int    `json:"state"`
+	Msg   string `json:"msg"`
+}
+
+type SessionInfo struct {
+	Status SessionStatus     `json:"status"`
+	Data   map[string]string `json:"data"`
+}
+
+func SetSessionByToken(token string) string {
+	environment := os.Getenv("GOENV")
+	url := "https://xmall_test.codoon.com/xmall/tokensession?token=%s"
+	if environment == "ONLINE" {
+		url = "https://xmall.codoon.com/xmall/tokensession?token=%s"
+	}
+	next_url := fmt.Sprintf(url, token)
+	res, err := http.Get(next_url)
+	if err != nil {
+		fmt.Println("token-session connection error.")
+		return ""
+	}
+	cookie := res.Cookies()
+	for _, item := range cookie {
+		if item.Name == "sessionid" {
+			return item.Value
+		}
+	}
+	return ""
+}
+
+func GetUserIdBySession(sessionId string) string {
+	environment := os.Getenv("GOENV")
+	url := "https://xmall_test.codoon.com/xmall/get_userid_by_sessionid?sessionid=%s"
+	if environment == "ONLINE" {
+		url = "https://xmall.codoon.com/xmall/get_userid_by_sessionid?sessionid=%s"
+	}
+	next_url := fmt.Sprintf(url, sessionId)
+	res, err := http.Get(next_url)
+	if err != nil {
+		fmt.Println("token-session connection error.")
+		return ""
+	}
+	defer res.Body.Close()
+	ret := SessionInfo{}
+	result, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("session_user fetch response failed.")
+		return ""
+	}
+	fmt.Println(result)
+	err = json.Unmarshal(result, &ret)
+	fmt.Println(ret)
+	return ret.Data["user_id"]
+}
