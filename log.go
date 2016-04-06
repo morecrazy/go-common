13bee4d9f83ca3934f1f996ba50bc5d1ae8e1595
zhangjl@codoon.com <codoon@CodoondeMacBook-Pro.local>
2016-04-06 11:21:55 +0800

    日志分隔 1g 一个文件

--- log.go
+++ log.go
@@ -3,7 +3,6 @@ package common
 import (
 	"fmt"
 	"log"
-	"os"
 	"third/go-logging"
 )
 
@@ -19,7 +18,7 @@ func InitLogger(process_name, format_str string) (*logging.Logger, error) {
 
 	Logger = logging.MustGetLogger(process_name)
 	//sql_log_fp, err := os.OpenFile(Config.LogDir+"/"+process_name+".log.mysql", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-	sql_log_fp, err := logging.NewFileLogWriter(Config.LogDir+"/"+process_name+".log.mysql",true,2000)
+	sql_log_fp, err := logging.NewFileLogWriter(Config.LogDir+"/"+process_name+".log.mysql",true, 1024 * 1024 * 1024)
 	if err != nil {
 		fmt.Println("open file[%s.mysql] failed[%s]", Config.LogFile, err)
 		return nil, err
@@ -28,18 +27,14 @@ func InitLogger(process_name, format_str string) (*logging.Logger, error) {
 	MysqlLogger = log.New(sql_log_fp, "", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
 
 	//info_log_fp, err := os.OpenFile(Config.LogDir+"/"+process_name+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-	info_log_fp, err := logging.NewFileLogWriter(Config.LogDir+"/"+process_name+".log",true,2000)
+	info_log_fp, err := logging.NewFileLogWriter(Config.LogDir+"/"+process_name+".log",true, 1024 * 1024 * 1024)
 	if err != nil {
 		fmt.Println("open file[%s] failed[%s]", Config.LogFile, err)
 		return nil, err
-	}else {
-		fmt.Println("open 11111111")
 	}
-	for i := 0; i < 10000000; i++ {
-		info_log_fp.Write([]byte("adsfawefaewfawefawef"))
-	}
+
 	//err_log_fp, err := os.OpenFile(Config.LogDir+"/"+process_name+".log.wf", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-	err_log_fp, err := logging.NewFileLogWriter(Config.LogDir+"/"+process_name+".log.wf",true,2000)
+	err_log_fp, err := logging.NewFileLogWriter(Config.LogDir+"/"+process_name+".log.wf",true, 1024 * 1024 * 1024)
 	if err != nil {
 		fmt.Println("open file[%s.wf] failed[%s]", Config.LogFile, err)
 		return nil, err
