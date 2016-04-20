7130dbbebe20d5889f6fba84bd90d31912a80136
彭近尧 <Archilleus@jiangfudayin.local>
2016-04-20 16:40:46 +0800

    debug

--- session_client.go
+++ session_client.go
@@ -19,9 +19,7 @@ type SessionInfo struct {
 }
 
 func SetSessionByToken(token string) string {
-	environment := os.Getenv("GOENV")
 	url := "https://xmall.codoon.com/xmall/tokensession?token=%s"
-
 	next_url := fmt.Sprintf(url, token)
 	res, err := http.Get(next_url)
 	if err != nil {
@@ -38,7 +36,6 @@ func SetSessionByToken(token string) string {
 }
 
 func GetUserIdBySession(sessionId string) string {
-	environment := os.Getenv("GOENV")
 	url := "https://xmall.codoon.com/xmall/get_userid_by_sessionid?sessionid=%s"
 	next_url := fmt.Sprintf(url, sessionId)
 	res, err := http.Get(next_url)
