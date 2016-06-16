c23339df5192b412d7ce5d175f5124f5ae97d272
baixiao <baixiao@codoon.com>
2016-06-16 10:23:37 +0800

    InitLogger1

--- log.go
+++ log.go
@@ -70,6 +70,56 @@ func InitLogger(process_name string) (*logging.Logger, error) {
 	return Logger, err
 }
 
+func InitLogger1(process_name, format_str string) (*logging.Logger, error) {
+
+	if Logger != nil {
+		return nil, nil
+	}
+
+	Logger = logging.MustGetLogger(process_name)
+
+	info_log_fp, err := logging.NewFileLogWriter(Config.LogDir+"/"+process_name+".log", false, 1024*1024*1024)
+	if err != nil {
+		fmt.Println("open file[%s] failed[%s]", Config.LogFile, err)
+		return nil, err
+	}
+
+	err_log_fp, err := logging.NewFileLogWriter(Config.LogDir+"/"+process_name+".log.wf", false, 1024*1024*1024)
+	if err != nil {
+		fmt.Println("open file[%s.wf] failed[%s]", Config.LogFile, err)
+		return nil, err
+	}
+
+	backend_info := logging.NewLogBackend(info_log_fp, "", 0)
+	backend_err := logging.NewLogBackend(err_log_fp, "", 0)
+	format := logging.MustStringFormatter(format_str)
+	backend_info_formatter := logging.NewBackendFormatter(backend_info, format)
+	backend_err_formatter := logging.NewBackendFormatter(backend_err, format)
+
+	backend_info_leveld = logging.AddModuleLevel(backend_info_formatter)
+	switch Config.LogLevel {
+	case "ERROR":
+		backend_info_leveld.SetLevel(logging.ERROR, "")
+	case "WARNING":
+		backend_info_leveld.SetLevel(logging.WARNING, "")
+	case "NOTICE":
+		backend_info_leveld.SetLevel(logging.NOTICE, "")
+	case "INFO":
+		backend_info_leveld.SetLevel(logging.INFO, "")
+	case "DEBUG":
+		backend_info_leveld.SetLevel(logging.DEBUG, "")
+	default:
+		backend_info_leveld.SetLevel(logging.ERROR, "")
+	}
+
+	backend_err_leveld := logging.AddModuleLevel(backend_err_formatter)
+	backend_err_leveld.SetLevel(logging.WARNING, "")
+
+	logging.SetBackend(backend_info_leveld, backend_err_leveld)
+
+	return Logger, err
+}
+
 func ChangeLogLevel(LogLevel string) {
 	switch LogLevel {
 	case "ERROR":
