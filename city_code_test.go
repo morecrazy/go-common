c5fddb3ba22c12112c40e24666aca9640ca07e6d
liudanking <liudanking@gmail.com>
2016-07-21 15:16:11 +0800

    add function GetCodeFromCity

---
+++ city_code_test.go
@@ -0,0 +1,27 @@
+package common
+
+import (
+	"log"
+
+	"testing"
+)
+
+func TestGenCityCode(t *testing.T) {
+	NAME2CODE := map[string]string{}
+	for k, v := range CODE2NAME {
+		NAME2CODE[v] = k
+	}
+	log.Printf("NAME2CODE:%#v", NAME2CODE)
+
+	MDCG_CITY_CODE := map[string]string{}
+	for k, v := range MDCG_CITY {
+		MDCG_CITY_CODE[v] = k
+	}
+	log.Printf("MDCG_CITY_CODE:%#v", MDCG_CITY_CODE)
+
+	OTHER_CITY_CODE := map[string]string{}
+	for k, v := range OTHER_CITY {
+		OTHER_CITY_CODE[v] = k
+	}
+	log.Printf("OTHER_CITY_CODE:%#v", OTHER_CITY_CODE)
+}
