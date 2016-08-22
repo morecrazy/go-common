fb77098b40b87a800675a2984f7747fba33e1466
zhanghan <zhanghan@codoon.com>
2016-08-22 18:08:51 +0800

    fix return bug

--- gin_handler.go
+++ gin_handler.go
@@ -193,6 +193,7 @@ func GinLogTracer(srvName, srvCode, name string) gin.HandlerFunc {
 	brokers, err := LoadContentFromEtcd([]string{"http://etcd.in.codoon.com:2379"}, name, "/online")
 	if err != nil {
 		log.Printf("Fetch kafka log brokers error:%v", err)
+		return nil
 	}
 
 	bytes := []byte(brokers)
