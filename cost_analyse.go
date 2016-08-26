9188cd2c41899897dca4c930eacb2afa861094ee
baixiao <baixiao@codoon.com>
2016-08-26 10:21:26 +0800

    耗时分析

---
+++ cost_analyse.go
@@ -0,0 +1,43 @@
+package common
+
+import (
+	"time"
+	"fmt"
+)
+
+type CostAnalyse struct {
+	Name string
+	At   time.Time
+}
+
+func InitCostAnalyse(node string) []CostAnalyse {
+	return []CostAnalyse{CostAnalyse{
+		Name: node,
+		At: time.Now(),
+	}}
+}
+
+func AddCostNode(node string, costList []CostAnalyse) []CostAnalyse {
+	costNode := CostAnalyse{
+		Name: node,
+		At: time.Now(),
+	}
+	return append(costList, costNode)
+}
+
+func PrintCostAnalyse(costList []CostAnalyse) {
+	if len(costList) < 2 {
+		return
+	}
+
+	var str string
+	for i, costNode := range costList {
+		if i == len(costList) - 1 {
+			break
+		}
+		nextNode := costList[i + 1]
+		costTime := nextNode.At.Sub(costNode.At).Nanoseconds()/1000000
+		str += fmt.Sprintf("[%s-%s:%d ms] ", nextNode.Name, costNode.Name, costTime)
+	}
+	Noticef("%s", str)
+}
