1764d12bb4def5976a2f7980520d4fb24f8a4b79
liudanking <liudanking@gmail.com>
2016-09-05 17:52:57 +0800

    add goroutine/gc to statsd

--- ging_statsd_handler_test.go
+++ ging_statsd_handler_test.go
@@ -9,10 +9,13 @@ import (
 
 func TestGinStatsdHandler(t *testing.T) {
 	addr := fmt.Sprintf("http://%s/hi", GIN_SERVER_ADDR)
-
+	params := map[string]string{
+		"user_id": "abc",
+		"foo":     "foo",
+	}
 	for i := 0; i < 70; i++ {
 		log.Printf("request:%d", i)
-		data, err := HttpRequest("POST", addr, nil)
+		data, err := HttpRequest("POST", addr, params)
 		if err != nil {
 			log.Fatal(err)
 		} else {
