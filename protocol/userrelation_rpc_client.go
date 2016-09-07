891ed051605ecb9940230648dd76daace519fc4d
duhb <duhb@codoon.com>
2016-09-07 11:59:46 +0800

    no message

--- protocol/userrelation_rpc_client.go
+++ protocol/userrelation_rpc_client.go
@@ -23,12 +23,22 @@ type FollowingRes struct {
 	Data bool
 }
 
+type GetCountRes struct {
+	Data int
+}
+
+type GetFlagsRes struct {
+	Data map[string]int
+}
+
 var UserRelationRpcFuncMap map[string]string = map[string]string{
 	"follow":              "UserRelationHandler.FollowPeople",
 	"unfollow":            "UserRelationHandler.UnFollowPeople",
 	"get_follower":        "UserRelationHandler.GetFollower",
 	"get_following_flag":  "UserRelationHandler.GetFollowingFlag",
-	"get_following_count": "UserRelationHandler.GetFollowedCount",
+	"get_following_count": "UserRelationHandler.GetFollowingCount",
+	"get_follower_count":  "UserRelationHandler.GetFollowedCount",
+	"get_friends_count":   "UserRelationHandler.GetFriendsCount",
 	"get_following_flags": "UserRelationHandler.GetFollowingFlags",
 	"get_following":       "UserRelationHandler.GetFollowing",
 	"get_friends":         "UserRelationHandler.GetFriends",
